0d5875598bb257048232732c8be9786cc381563f
Yamsat <50951444+Yamsat@users.noreply.github.com>
2022-02-12 14:58:45 +1100

    import statement for ownable removed

--- nft_marketplace.sol
+++ nft_marketplace.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.5.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/ownership/Ownable.sol";
 
 contract TokenFactory is ERC721Full {
     using Counters for Counters.Counter;
